a06733539267b4b0f5e86dd23ba254209f24d3ec
Lex <alex.frank84@gmail.com>
2022-06-14 10:46:18 +0200

    Fix flaky end2end test

--- app/src/androidTest/java/de/gnarly/got/EndToEndTest.kt
+++ app/src/androidTest/java/de/gnarly/got/EndToEndTest.kt
@@ -1,32 +1,26 @@
 package de.gnarly.got
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
 
 // improvement mock third party services
+@OptIn(ExperimentalCoroutinesApi::class)
 class EndToEndTest {
 	@get:Rule
 	val composeTestRule = createAndroidComposeRule<MainActivity>()
 
 	@Test
-	fun endToEnd(): Unit = runBlocking {
+	fun endToEnd(): Unit = runTest {
 		composeTestRule.setContent {
 			MainScreen()
 		}
 
-		composeTestRule.waitUntil(5_000) {
-			try {
-				composeTestRule.onNodeWithTag("house 1").assertIsDisplayed()
-				true
-			} catch (e: Exception) {
-				false
-			}
+		composeTestRule.waitUntil(10_000) {
+			composeTestRule.onAllNodesWithTag("house 1").fetchSemanticsNodes().size == 1
 		}
 		composeTestRule.onNodeWithTag("house 1").performClick()
 		composeTestRule.awaitIdle()
