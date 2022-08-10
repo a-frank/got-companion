c68e3ea5fc616cae1722c1d9e06379bbe6f207e6
Lex <alex.frank84@gmai.com>
2022-08-10 15:06:04 +0200

    Made db and daos singelton

--- app/src/main/java/de/gnarly/got/database/DbModule.kt
+++ app/src/main/java/de/gnarly/got/database/DbModule.kt
@@ -7,20 +7,24 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DbModule {
 	@Provides
+	@Singleton
 	fun provideGoTDatabase(@ApplicationContext context: Context): GoTDatabase =
 		Room.databaseBuilder(context, GoTDatabase::class.java, "got.db")
 			.build()
 
 	@Provides
+	@Singleton
 	fun provideHouseDao(db: GoTDatabase): HouseDao =
 		db.getHouseDao()
 
 	@Provides
+	@Singleton
 	fun provideCharacterDao(db: GoTDatabase): CharacterDao =
 		db.getCharacterDao()
 }
